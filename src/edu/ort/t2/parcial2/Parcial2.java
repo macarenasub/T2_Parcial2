1562c7300c3bdcbb1895a8b7eda20f0327009dfc
Macarena Ugarte [Mac] <macarenasub@gmail.com>
2018-06-25 17:46:40 -0300

    final

--- src/edu/ort/t2/parcial2/Parcial2.java
+++ src/edu/ort/t2/parcial2/Parcial2.java
@@ -1,6 +1,5 @@
 package edu.ort.t2.parcial2;
 
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Parcial2 {
@@ -9,17 +8,20 @@ public class Parcial2 {
 		// TODO Auto-generated method stub
 
 		Scanner input = new Scanner(System.in);
+		
 		TPosiciones rd = new TPosiciones();
+		
 		rd.procesarInfo(5);
+		
+		System.out.println("Resultado final campeonato: ");
+		
 		rd.verTodo();
-
-		System.out.println("Resultado final campeonato");
 
 		String nombre = "";
 		boolean ok = false;
 		do {
 			ok = true;
-			System.out.println("Ingrese el nombre del equipo: ");
+			System.out.print("Ingrese el nombre del equipo: ");
 			nombre = input.nextLine();
 			if (!nombre.matches("\\D+")) {
 				System.out.println("Error ingresar caracteres entre A-Z.");
@@ -27,12 +29,10 @@ public class Parcial2 {
 			}
 
 		} while (!ok);
-		
-		System.out.println(nombre);
 
 		System.out.println(rd.procesarDatosEquipo(nombre));
 
-		System.out.println(rd.procesarEquipo(nombre, "Gan"));
+		//System.out.println(rd.procesarEquipo(nombre, "Gan"));
 
 		System.out.println(rd.procesarEquipo("real madrid", "Gan"));
 
